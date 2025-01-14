a7ea53221e499c87d9ce8c0bd3dc3fdd17fd05e3
pjprobst <111828284+pjprobst@users.noreply.github.com>
2025-01-14 18:52:43 +0000

    delete back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -303,19 +303,6 @@
     }
     
 
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
-
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
